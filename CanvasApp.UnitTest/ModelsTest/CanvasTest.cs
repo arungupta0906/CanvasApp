63a7ea22ef04cb8722041576ec477de3d627a146
arun.i.gupta <arun.i.gupta@WIN-N25Z247M8M>
2018-07-19 00:45:49 +0800

    Added unit test for canvas

---
+++ CanvasApp.UnitTest/ModelsTest/CanvasTest.cs
@@ -0,0 +1,21 @@
+﻿using CanvasApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CanvasApp.UnitTest.ModelsTest
+{
+    public class CanvasTest
+    {
+        [Fact]
+        public void Canvas_CreateObject()
+        {
+            uint width = 20;
+            uint height = 10;
+            Canvas canvas = new Canvas(width, height);
+            Assert.NotNull(canvas);
+            Assert.Equal(200, canvas.Cells.Length);
+        }
+    }
+}
